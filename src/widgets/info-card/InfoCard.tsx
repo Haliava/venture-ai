7bfe9b928706a7743225030a620d87b58a26ec3c
ivan <frontman@bwg-io.site>
2025-04-09 10:56:56 +0300

    fix: deploy err

--- src/widgets/info-card/InfoCard.tsx
+++ src/widgets/info-card/InfoCard.tsx
@@ -1,27 +1,23 @@
-// import { ReactNode } from "react";
-// // @ts-ignore
-// import classnames from "classnames";
-// import styles from './styles.module.scss';
+import { ReactNode } from "react";
+import styles from './styles.module.scss';
 
-// export type InfoCardProps = {
-//   title: string;
-//   imageSrc: string;
-//   imagePosition: 'left' | 'right';
-//   children?: ReactNode;
-//   className?: string;
-//   contentClassName?: string;
-// }
+export type InfoCardProps = {
+  title: string;
+  imageSrc: string;
+  imagePosition: 'left' | 'right';
+  children?: ReactNode;
+  className?: string;
+  contentClassName?: string;
+}
 
-// export const InfoCard = ({ imagePosition, imageSrc, title, children, className, contentClassName }: InfoCardProps) => {
-//   return (
-//     // @ts-ignore
-//     <div className={classnames(styles.root, className, {[styles.imageRight]: imagePosition === 'right'})}>
-//       {/* // @ts-ignore */}
-//       <img className={classnames(styles.image)} src={imageSrc} />
-//       <div className={contentClassName}>
-//         <h3 className="text-[20px] font-semibold">{title}</h3>
-//         {children}
-//       </div>
-//     </div>
-//   )
-// }
+export const InfoCard = ({ imagePosition, imageSrc, title, children, className, contentClassName }: InfoCardProps) => {
+  return (
+    <div className={`${styles.root} ${className} ${imagePosition === 'right' ? 'styles.imageRight': ''}`}>
+      <img className={`${styles.image}`} src={imageSrc} />
+      <div className={contentClassName}>
+        <h3 className="text-[20px] font-semibold">{title}</h3>
+        {children}
+      </div>
+    </div>
+  )
+}
