48e8af609d580ab1f697ea64195d378094f44c9d
ivan <frontman@bwg-io.site>
2025-04-09 10:42:33 +0300

    fix: deploy err

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      'react/jsx-runtime': require.resolve('react/jsx-runtime'),
     },
   },
   build: {
