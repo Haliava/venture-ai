7b6746e9de63d96e60373bc723a4bf0b5faa9cb6
ivan <frontman@bwg-io.site>
2025-04-09 10:45:14 +0300

    fix: deploy err

--- src/widgets/info-card/InfoCard.tsx
+++ src/widgets/info-card/InfoCard.tsx
@@ -1,27 +1,27 @@
-import { ReactNode } from "react";
-// @ts-ignore
-import classnames from "classnames";
-import styles from './styles.module.scss';
+// import { ReactNode } from "react";
+// // @ts-ignore
+// import classnames from "classnames";
+// import styles from './styles.module.scss';
 
-export type InfoCardProps = {
-  title: string;
-  imageSrc: string;
-  imagePosition: 'left' | 'right';
-  children?: ReactNode;
-  className?: string;
-  contentClassName?: string;
-}
+// export type InfoCardProps = {
+//   title: string;
+//   imageSrc: string;
+//   imagePosition: 'left' | 'right';
+//   children?: ReactNode;
+//   className?: string;
+//   contentClassName?: string;
+// }
 
-export const InfoCard = ({ imagePosition, imageSrc, title, children, className, contentClassName }: InfoCardProps) => {
-  return (
-    // @ts-ignore
-    <div className={classnames(styles.root, className, {[styles.imageRight]: imagePosition === 'right'})}>
-      {/* // @ts-ignore */}
-      <img className={classnames(styles.image)} src={imageSrc} />
-      <div className={contentClassName}>
-        <h3 className="text-[20px] font-semibold">{title}</h3>
-        {children}
-      </div>
-    </div>
-  )
-}
+// export const InfoCard = ({ imagePosition, imageSrc, title, children, className, contentClassName }: InfoCardProps) => {
+//   return (
+//     // @ts-ignore
+//     <div className={classnames(styles.root, className, {[styles.imageRight]: imagePosition === 'right'})}>
+//       {/* // @ts-ignore */}
+//       <img className={classnames(styles.image)} src={imageSrc} />
+//       <div className={contentClassName}>
+//         <h3 className="text-[20px] font-semibold">{title}</h3>
+//         {children}
+//       </div>
+//     </div>
+//   )
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      'react/jsx-runtime': require.resolve('react/jsx-runtime'),
     },
   },
   build: {
