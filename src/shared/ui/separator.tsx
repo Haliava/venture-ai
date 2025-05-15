d963df34546a961f7d82675401097721437a1a50
ivan <frontman@bwg-io.site>
2025-05-16 00:51:15 +0300

    fix: build error

--- src/shared/ui/separator.tsx
+++ src/shared/ui/separator.tsx
@@ -1,14 +1,4 @@
-import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
-import { cn } from "@/shared/lib/utils"
-
-function Separator({
-  className,
-  orientation = "horizontal",
-  decorative = true,
-  ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+function Separator() {
   return (
     <span className="border border-white w-[100vw] -ml-5 mt-8 lg:w-[82vw] xl:w-[69vw]" />
   )
